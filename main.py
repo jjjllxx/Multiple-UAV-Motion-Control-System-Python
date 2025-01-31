252cceca10c4f274087f39359cade75e2de8a8e2
jlx321@126.com <jlx321@126.com>
2025-01-31 11:29:41 +0800

    main: update according to new rrt functions

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import yaml
-import lazy_rrt
-
+import rrt_lazy
+import rrt_connect
+import rrt_extend
+import rrt_star
 
 def main():
     with open("config.yaml", "r") as file:
         config = yaml.safe_load(file)
 
     if config['rrt_type'] == 'lazy':
-        lazy_rrt.run_lazy_rrt(config['step_size'], config['is_random_world'])
+        rrt_lazy.run_lazy_rrt(config['step_size'], config['is_random_world'])
 
     elif config['rrt_type'] == 'connect':
-        pass
+        rrt_connect.run_rrt_connect(config['step_size'], config['is_random_world'])
+
     elif config['rrt_type'] == 'extend':
-        pass
+        rrt_extend.run_rrt_extend(config['step_size'], config['is_random_world'])
+
     elif config['rrt_type'] == 'star':
-        pass
+        rrt_star.run_rrt_star(config['step_size'], config['is_random_world'], config['samples'])
+
     else:
         print("Invalid RRT type!")
 
