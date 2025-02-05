b48c0cb979261917b1833aa23c9341ce1657480b
random name <60777462+jjjllxx@users.noreply.github.com>
2025-02-05 14:34:30 +0800

    main: example code to run each function

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 import yaml
-import rrt_lazy
-import rrt_connect
-import rrt_extend
-import rrt_star
+import rrt_lib
+import traj_lib
+import plot_lib
+import structs
+import random
+
+def initialise_uavs(uav_num, world: structs.World):
+    uavs = []
+    base_width = world.width * 0.1
+    base_length = world.length * 0.1
+    base_height = world.height * 0.1
+    
+    for i in range(uav_num):
+        start_pt = structs.Point(base_width * random.random(), 
+                                 base_length * random.random(), 
+                                 base_height * random.random())
+        
+        end_pt = structs.Point(base_width * (random.random() + 9), 
+                               base_length * (random.random() + 9), 
+                               base_height * (random.random() + 9))
+
+        uavs.append(structs.UAV(start_pt, end_pt))
+
+    return uavs
 
 def main():
     with open("config.yaml", "r") as file:
         config = yaml.safe_load(file)
 
-    if config['rrt_type'] == 'lazy':
-        rrt_lazy.run_lazy_rrt(config['step_size'], config['is_random_world'])
+    world = rrt_lib.initialise_world(config)
+    uavs = initialise_uavs(config["uav_num"], world)
 
-    elif config['rrt_type'] == 'connect':
-        rrt_connect.run_rrt_connect(config['step_size'], config['is_random_world'])
+    paths = []
+    trajs = []
+    for uav in uavs:
+        paths.append(rrt_lib.find_path_by_rrt(uav, world, config))
+        key_pts = traj_lib.find_key_pts(paths[-1])
+        trajs.append(traj_lib.generate_traj(key_pts))
 
-    elif config['rrt_type'] == 'extend':
-        rrt_extend.run_rrt_extend(config['step_size'], config['is_random_world'])
+    for path in paths:
+        plot_lib.plot_result(world, path)
 
-    elif config['rrt_type'] == 'star':
-        rrt_star.run_rrt_star(config['step_size'], config['is_random_world'], config['samples'])
+    plot_lib.plot_traj_xyz(trajs)
 
-    else:
-        print("Invalid RRT type!")
+    plot_lib.save_traj_gif(world, trajs)
 
 if __name__ == '__main__':
     main()
